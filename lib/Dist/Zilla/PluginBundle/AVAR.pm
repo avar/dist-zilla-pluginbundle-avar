f72f66f74a251ed841c1637a648f141b333ce8f5
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-03-27 21:05:41 +0000

    Next release. See Changes

--- lib/Dist/Zilla/PluginBundle/AVAR.pm
+++ lib/Dist/Zilla/PluginBundle/AVAR.pm
@@ -13,6 +13,7 @@ use Dist::Zilla::Plugin::AutoPrereq;
 use Dist::Zilla::Plugin::MetaNoIndex;
 use Dist::Zilla::Plugin::ReadmeFromPod;
 use Dist::Zilla::Plugin::OverridableMakeMaker;
+use Dist::Zilla::Plugin::CompileTests;
 
 sub bundle_config {
     my ($self, $section) = @_;
@@ -22,7 +23,8 @@ sub bundle_config {
     my $ldist       = lc $dist;
     my $github_user = $args->{github_user} // 'avar';
     my $no_a_pre    = $args->{no_AutoPrereq} // 0;
-    my $no_mm       = $args->{no_MakeMaker} // 1;
+    my $use_mm      = $args->{use_MakeMaker} // 1;
+    my $use_ct      = $args->{use_CompileTests} // 1;
     my $bugtracker  = $args->{bugtracker}  // 'github';
     my $tracker;
 
@@ -80,10 +82,15 @@ sub bundle_config {
             }
         ],
 
-        # My very own MakeMaker
-        ($no_mm
+        # Maybe use MakeMaker, maybe not
+        ($use_mm
          ? ([ MakeMaker  => { } ])
-         : ([ OverridableMakeMaker  => { } ])),
+         : ()),
+
+        # Maybe CompileTests
+        ($use_ct
+         ? ([ CompileTests  => { } ])
+         : ()),
     );
     push @plugins, @extra;
 
